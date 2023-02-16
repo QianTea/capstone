356db3c1121bcfc221b0521af8a52b33905305c4
Yifen Lin <71994066+QianTea@users.noreply.github.com>
2023-02-16 18:24:56 -0500

    admin panel - add addProduct

--- react-project/src/App.js
+++ react-project/src/App.js
@@ -4,14 +4,15 @@ import './App.css';
 import { NavBar } from './shared/components/navbar';
 import ResponsiveAppBar from './shared/components/NavBar2';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Product from './products/product';
+import Product from './menu/product';
 import Home from './home/home';
 import NoPage from './shared/nopage';
 import Layout from './shared/layout';
-import Detail from './products/detail';
+import Detail from './menu/detail';
 import Login from './login/login';
 import DashboardContent from './home/dashboard';
 import Website from './website/website';
+import AddProduct from './menu/addProduct';
 function App() {
   return (
     <BrowserRouter>
@@ -22,6 +23,7 @@ function App() {
           <Route path="/website" element={<Website />} />
           <Route path='/products' element={<Product />} />
             <Route path="/products/:id" element={<Detail />} />
+            <Route path="/products/addProduct" element={<AddProduct />} />
           <Route path="*" element={<NoPage />} />
           </Route>
         </Routes>
--- react-project/src/website/website.js
+++ react-project/src/website/website.js
@@ -8,15 +8,15 @@ import { styled, createTheme, ThemeProvider } from '@mui/material/styles';
 import Container from '@mui/material/Container';
 import Paper from '@mui/material/Paper';
 import Grid from '@mui/material/Grid';
-
+import Item from '@mui/material/Grid';
 
 const mdTheme = createTheme();
-
 
 const Website = () => {
     return (
         <ThemeProvider theme={mdTheme}>
-            <Box sx={{ display: 'flex' }}>
+            <Box sx={{ display: 'flex' ,
+        '& .MuiTextField-root': { m: 1, width: '55ch' },}}>
                 <Box
                     component="form"
                     sx={{
@@ -35,83 +35,87 @@ const Website = () => {
                         <Grid >
                             <h1>Store Info</h1>
                             <form name="addItem" >
-                                <Grid container spacing={2}>
-                                    <Grid item xs={12} md={4} lg={3}>
-                                        <img src="./logo.png" alt="logo image" />
-                                        <div>
-                                            <input type="file" />
-                                        </div>
+                                <Grid container spacing={1}>
+                                    <Grid item xs={12} md={4} lg={4}>
+                                        <Item>
+                                            <img src="./logo.png" alt="logo image" />
+                                            <div>
+                                                <input type="file" />
+                                            </div>
+                                        </Item>
                                     </Grid>
-                                    <Grid item xs={12} md={8} lg={9}>
-                                        <div>
-                                            <TextField
-                                                id="storeName"
-                                                label="storeName"
-                                                variant="filled"
-                                                defaultValue="Riverside Fish Hut"
-                                            />
-                                        </div>
-                                        <div>
-                                            <TextField
-                                                id="storeDescription"
-                                                label="Description"
-                                                multiline
-                                                rows={5}
-                                                variant="filled"
-                                                fullWidth
-                                                defaultValue="Founded in 2012, Riverside Fish Hut is known for 
+                                    <Grid item xs={12} md={8} lg={8}>
+                                        <Item>
+                                            <div>
+                                                <TextField
+                                                    id="storeName"
+                                                    label="storeName"
+                                                    variant="filled"
+                                                    defaultValue="Riverside Fish Hut"
+                                                />
+                                            </div>
+                                            <div>
+                                                <TextField
+                                                    id="storeDescription"
+                                                    label="Description"
+                                                    multiline
+                                                    rows={5}
+                                                    variant="filled"
+                                                    fullWidth
+                                                    defaultValue="Founded in 2012, Riverside Fish Hut is known for 
                                                     revolutionizing the way people eat and enjoy food.
                                                     Serving classic fish and chips in a cozy riverside setting, 
                                                     with a patio during the summer! Follow us on Facebook!"
-                                            />
-                                        </div>
-                                        <div>
-                                            <TextField
-                                                id="phone"
-                                                label="Phone"
-                                                variant="filled"
-                                                size="small"
+                                                />
+                                            </div>
+                                            <div>
+                                                <TextField
+                                                    id="phone"
+                                                    label="Phone"
+                                                    variant="filled"
+                                                    size="small"
 
-                                                defaultValue="(519) 653-0788"
-                                            />
-                                        </div>
-                                        <div>
-                                            <TextField
-                                                id="orderLink"
-                                                label="Online Order Link"
-                                                variant="filled"
-                                                size="small"
-                                                multiline
-                                                fullWidth
-                                                defaultValue="https://www.skipthedishes.com/riverside-fish-hut"
-                                            />
-                                        </div>
-                                        <div>
-                                            <TextField
-                                                id="address"
-                                                label="Address"
-                                                variant="filled"
-                                                multiline
-                                                fullWidth
-                                                defaultValue="Unit-B 157 King Street West,Cambridge, ON N3H 1B5,Canada"
-                                            />
-                                        </div>
-                                        <div>Business Hours:
-                                            <TextField
-                                                id="businessHour"
-                                                label="Business Hours"
-                                                variant="filled"
-                                                size="small"
-                                                defaultValue="Monday closed"
-                                            />
-                                        </div>
-                                        <p></p>
-                                        <Button
-                                            variant="contained"
-                                            startIcon={<SendIcon />}
-                                            color="success">
-                                            Submit
-                                        </Button>
+                                                    defaultValue="(519) 653-0788"
+                                                />
+                                            </div>
+                                            <div>
+                                                <TextField
+                                                    id="orderLink"
+                                                    label="Online Order Link"
+                                                    variant="filled"
+                                                    size="small"
+                                                    multiline
+                                                    fullWidth
+                                                    defaultValue="https://www.skipthedishes.com/riverside-fish-hut"
+                                                />
+                                            </div>
+                                            <div>
+                                                <TextField
+                                                    id="address"
+                                                    label="Address"
+                                                    variant="filled"
+                                                    multiline
+                                                    fullWidth
+                                                    defaultValue="Unit-B 157 King Street West,Cambridge, ON N3H 1B5,Canada"
+                                                />
+                                            </div>
+                                            <div>Business Hours:
+                                                <TextField
+                                                    id="businessHour"
+                                                    label="Business Hours"
+                                                    variant="filled"
+                                                    size="small"
+                                                    defaultValue="Monday closed"
+                                                />
+                                            </div>
+                                            <p></p>
+                                            <Button
+                                                variant="contained"
+                                                startIcon={<SendIcon />}
+                                                color="success">
+                                                Submit
+                                            </Button>
+                                        </Item>
                                     </Grid>
                                 </Grid>
                             </form>
