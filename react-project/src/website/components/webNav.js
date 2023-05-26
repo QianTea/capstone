eb411ccd06258f170f7edc017536911fca9ade77
Yifen Lin <71994066+QianTea@users.noreply.github.com>
2023-05-26 13:54:42 -0400

    web nav - tab

--- react-project/src/website/components/webNav.js
+++ react-project/src/website/components/webNav.js
@@ -4,23 +4,30 @@ import axios from 'axios';
 import { Link } from 'react-router-dom';
 // mui
 import { createTheme, ThemeProvider } from '@mui/material/styles';
+import Box from '@mui/material/Box';
+import Tabs from '@mui/material/Tabs';
+import Tab from '@mui/material/Tab';
+//icons
+import StorefrontTwoToneIcon from '@mui/icons-material/StorefrontTwoTone';
+import SetMealTwoToneIcon from '@mui/icons-material/SetMealTwoTone';
+import InfoTwoToneIcon from '@mui/icons-material/InfoTwoTone';
 
 // style
 const mdTheme = createTheme();
 const styles = {
-    header: {
-        display: 'flex',
-        justifyContent: 'space-between',
-        alignItems: 'center',
-        backgroundColor: '#1B1B22',
-        padding: '1rem 2rem',
-        position: 'fixed',
-        top: 0,
-        left: 0,
-        right: 0,
-        zIndex: 999,
-        textAlign: 'left',
-    },
+    // header: {
+    //     display: 'flex',
+    //     justifyContent: 'space-between',
+    //     alignItems: 'center',
+    //     backgroundColor: '#1B1B22',
+    //     padding: '1rem 2rem',
+    //     position: 'fixed',
+    //     top: 0,
+    //     left: 0,
+    //     right: 0,
+    //     zIndex: 999,
+    //     textAlign: 'left',
+    // },
     logo: {
         flex: 1,
         height: '5rem',
@@ -32,14 +39,13 @@ const styles = {
         fontSize: '2rem',
         paddingLeft: '1rem',
         paddingBottom: '1rem',
-        
     },
     li: {
         display: 'flex',
-        flex: 1,
-        justifyContent: 'flex-end',
-        display: 'inline-block',
-        marginRight: '5rem',
+        // flex: 1,
+        // justifyContent: 'flex-end',
+        // display: 'inline-block',
+        // marginRight: '5rem',
     },
     a: {
         color: '#fff',
@@ -48,11 +54,21 @@ const styles = {
     },
 }
 
-
+function LinkTab(props) {
+    return (
+        <Tab
+            component="a"
+            onClick={(event) => {
+                event.preventDefault();
+            }}
+            {...props}
+        />
+    );
+}
 // top navigation bar of store website
 const WebNav = () => {
-        // get store info
-        const [storeInfo, setStoreInfo] = useState({});
+    // get store info
+    const [storeInfo, setStoreInfo] = useState({});
     useEffect(() => {
         const fetchData = async () => {
             const resultInfo = await axios.get('http://localhost:5500/website');
@@ -60,25 +76,28 @@ const WebNav = () => {
         };
         fetchData();
     }, [setStoreInfo]);
+    // tab things
+    const [value, setValue] = React.useState(0);
+
+    const handleChange = (event, newValue) => {
+        setValue(newValue);
+    };
     return (
         <ThemeProvider theme={mdTheme}>
             <div>
                 <nav style={styles.header}>
+
                     {/* logo image - link to home page*/}
-                    <span style={styles.logo}>
-                        <Link to="/home">
-                            <img style={styles.logo}
-                                alt={storeInfo.storeName}
-                                src={storeInfo.logoImage} />
-                        </Link>
-                        <Link to="/home" style={styles.name}>
+                    <Link to="/home">
+                        <img style={styles.logo}
+                            alt={storeInfo.storeName}
+                            src={storeInfo.logoImage} />
+                        <span style={styles.name}>
                             {storeInfo.storeName}
-                        </Link>
-                    </span>
+                        </span>
+                    </Link>
 
-
-                    <ul className="nav-links">
-
+                    <ul >
                         {/* link to home page */}
                         <li style={styles.li}>
                             <Link to="/home" style={styles.a}>
@@ -98,6 +117,32 @@ const WebNav = () => {
                             </Link>
                         </li>
                     </ul>
+                    <Box sx={{ width: '100%' }}>
+            <Tabs
+                value={value}
+                onChange={handleChange}
+                textColor="inherit"
+                indicatorColor="primary"
+                aria-label="nav tabs example"
+                centered
+            >
+                <LinkTab
+                    icon={<StorefrontTwoToneIcon />}
+                    label="Page Home"
+                    href="/drafts"
+                />
+                <LinkTab
+                    icon={<SetMealTwoToneIcon />}
+                    label="Page Menu"
+                    href="/trash"
+                />
+                <LinkTab
+                    icon={<InfoTwoToneIcon />}
+                    label="Page Contact"
+                    href="/spam"
+                />
+            </Tabs>
+        </Box>
                 </nav>
             </div>
         </ThemeProvider >
--- react-project/src/website/webLayout.js
+++ react-project/src/website/webLayout.js
@@ -1,6 +1,7 @@
 import { Outlet } from "react-router-dom";
 // Components
 import WebNav from "./components/webNav";
+import NavTabs from "./components/navTab";
 import WebFooter from "./pages/webFooter";
 // navigation
 import { useNavigate,useLocation } from "react-router-dom";
@@ -11,7 +12,7 @@ const styles = {
     page: {
         backgroundColor: '#24242D',
         color: '#fff',
-        textAlign: 'center',
+        // textAlign: 'center',
     },
     body: {
         backgroundColor: '#24242D',
@@ -29,12 +30,16 @@ const WebLayout = () => {
         if (currentPath == '/') navigate('/home');
     }, [navigate, location]);
     return (
-        < div style={styles.page}>
+        // < div style={styles.page}>
+        //     <WebNav />
+        //     <div style={styles.body}>
+        //         <Outlet />
+        //     </div>
+        //     <WebFooter sx={{ mt: 8, mb: 4 }} />
+        // </div>
+        <div style={styles.page}>
             <WebNav />
-            <div style={styles.body}>
-                <Outlet />
-            </div>
-            <WebFooter sx={{ mt: 8, mb: 4 }} />
+            <NavTabs/>
         </div>
     )
 }
